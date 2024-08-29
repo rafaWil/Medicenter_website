392c51f59a338e99fb980e0ef8ceaf8d1ecb11c4
Will Dev <rafaelmelo75831@gmail.com>
2024-08-28 23:02:53 -0300

    Update medicenter.js

--- assets/files/medicenter.js
+++ assets/files/medicenter.js
@@ -2,10 +2,14 @@
 window.onload = function() {
     //Select the button and include click event in menu
     document.querySelector(".menuMobile").addEventListener("click", function(){
+
         if(document.querySelector(".menu nav ul").style.display == 'flex'){
             document.querySelector(".menu nav ul").style.display = 'none';
+        
         } else { //Condition in IF for show other function in page
+        
             document.querySelector(".menu nav ul").style.display = 'flex';
+        
         }
     })
 }
