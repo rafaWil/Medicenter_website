1064ba130c63722a58a79a5470495e1f3cbb9313
Will Dev <rafaelmelo75831@gmail.com>
2024-08-28 23:02:05 -0300

    Function Javascript in page about click in menu mobile

---
+++ assets/files/medicenter.js
@@ -0,0 +1,11 @@
+/* Functions in page */
+window.onload = function() {
+    //Select the button and include click event in menu
+    document.querySelector(".menuMobile").addEventListener("click", function(){
+        if(document.querySelector(".menu nav ul").style.display == 'flex'){
+            document.querySelector(".menu nav ul").style.display = 'none';
+        } else { //Condition in IF for show other function in page
+            document.querySelector(".menu nav ul").style.display = 'flex';
+        }
+    })
+}
